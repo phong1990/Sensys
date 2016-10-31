96323c65681f33c0d4e0460d4f2f887d3c437c08
phong1990 <vu_minh_phong@yahoo.com.vn>
2016-10-31 17:03:39 -0600

    First Commit, creating the project

---
+++ src/main/java/usu/cs/Sensys/App.java
@@ -0,0 +1,13 @@
+package usu.cs.Sensys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
